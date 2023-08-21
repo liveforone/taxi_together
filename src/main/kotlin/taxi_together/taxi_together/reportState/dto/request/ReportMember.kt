77257d2b343cfd1b7fab5ea6640ed91b71d4ac44
liveforone <yc4852@gmail.com>
2023-08-21 21:53:48 +0900

    Add report member dto.

---
+++ src/main/kotlin/taxi_together/taxi_together/reportState/dto/request/ReportMember.kt
@@ -0,0 +1,6 @@
+package taxi_together.taxi_together.reportState.dto.request
+
+import jakarta.validation.constraints.NotNull
+import java.util.UUID
+
+data class ReportMember(@field:NotNull(message = "신고할 회원의 외부식별자를 입력하세요.") val memberUUID: UUID?)
