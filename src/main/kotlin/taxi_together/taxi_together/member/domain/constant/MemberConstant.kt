e47faed5038f809032f1e07ecb5fcb56236790fa
liveforone <yc4852@gmail.com>
2023-08-21 21:49:01 +0900

    Add member constant.

---
+++ src/main/kotlin/taxi_together/taxi_together/member/domain/constant/MemberConstant.kt
@@ -0,0 +1,8 @@
+package taxi_together.taxi_together.member.domain.constant
+
+object MemberConstant {
+    const val PW_TYPE = "VARCHAR(100)"
+    const val ROLE_TYPE = "VARCHAR(7)"
+    const val NICKNAME_TYPE = "VARCHAR(10)"
+    const val ADMIN_EMAIL = "admin_taxi_together@gmail.com"
+}
