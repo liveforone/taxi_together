61bee5d3617830fedd2cfa6eee9b91ba81a14540
liveforone <yc4852@gmail.com>
2023-08-23 16:49:08 +0900

    Refactor bankbook code structure.

--- src/main/kotlin/taxi_together/taxi_together/bankbook/domain/Bankbook.kt
+++ src/main/kotlin/taxi_together/taxi_together/bankbook/domain/Bankbook.kt
@@ -11,7 +11,10 @@ import taxi_together.taxi_together.member.domain.Member
 class Bankbook private constructor(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null,
     @OneToOne(fetch = FetchType.LAZY) @JoinColumn(updatable = false) @OnDelete(action = OnDeleteAction.CASCADE) val member: Member,
-    @Convert(converter = BankConverter::class) @Column(nullable = false, columnDefinition = BankbookConstant.BANK_TYPE) var bank: Bank,
+    @Convert(converter = BankConverter::class) @Column(
+        nullable = false,
+        columnDefinition = BankbookConstant.BANK_TYPE
+    ) var bank: Bank,
     @Column(nullable = false, columnDefinition = BankbookConstant.ACCOUNT_NUM_TYPE) var accountNumber: String,
 ) {
     companion object {
