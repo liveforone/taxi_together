256624b6ce2bf329b29294855af158883ed95978
liveforone <yc4852@gmail.com>
2023-08-24 17:07:20 +0900

    Add carpool application param

---
+++ src/main/kotlin/taxi_together/taxi_together/carpoolApplication/controller/constant/CarpoolApplicationParam.kt
@@ -0,0 +1,6 @@
+package taxi_together.taxi_together.carpoolApplication.controller.constant
+
+object CarpoolApplicationParam {
+    const val CARPOOL_UUID = "carpoolUUID"
+    const val MEMBER_UUID = "memberUUID"
+}
