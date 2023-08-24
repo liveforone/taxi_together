dcfce5cf9971bde60c859653541f2dd76a150ee8
liveforone <yc4852@gmail.com>
2023-08-24 17:07:13 +0900

    Add carpool application controller log.

---
+++ src/main/kotlin/taxi_together/taxi_together/carpoolApplication/controller/constant/CarpoolApplicationControllerLog.kt
@@ -0,0 +1,6 @@
+package taxi_together.taxi_together.carpoolApplication.controller.constant
+
+enum class CarpoolApplicationControllerLog(val log: String) {
+    CREATE_SUCCESS("카풀 신청 성공"),
+    CANCEL_SUCCESS("카풀 취소 성공")
+}
