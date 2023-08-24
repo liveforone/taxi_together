9b5ef023b5f4795088649a9568cb35e4b254bb7f
liveforone <yc4852@gmail.com>
2023-08-24 17:05:48 +0900

    Add carpool application pk.

---
+++ src/main/kotlin/taxi_together/taxi_together/carpoolApplication/domain/CarpoolApplicationPk.kt
@@ -0,0 +1,6 @@
+package taxi_together.taxi_together.carpoolApplication.domain
+
+import java.io.Serializable
+import java.util.*
+
+data class CarpoolApplicationPk(val carpoolUUID: UUID? = null, val memberUUID: UUID? = null) : Serializable
