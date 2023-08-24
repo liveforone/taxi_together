0489b5bda0c5399daca114436df40a75da9fb1f6
liveforone <yc4852@gmail.com>
2023-08-24 17:06:08 +0900

    Add carpool url.

---
+++ src/main/kotlin/taxi_together/taxi_together/carpool/controller/constant/CarpoolUrl.kt
@@ -0,0 +1,9 @@
+package taxi_together.taxi_together.carpool.controller.constant
+
+object CarpoolUrl {
+    const val DETAIL = "/carpool/detail/{uuid}"
+    const val SEARCH = "/carpool/search"
+    const val CREATE = "/carpool/create"
+    const val CALCULATE = "/carpool/calculate"
+    const val REMOVE = "/carpool/remove"
+}
