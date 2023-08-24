d820d44a9121f213aedd1a5f08cd8bfbb508826f
liveforone <yc4852@gmail.com>
2023-08-24 17:06:59 +0900

    Add carpool application constant.

---
+++ src/main/kotlin/taxi_together/taxi_together/carpoolApplication/domain/constant/CarpoolApplicationConstant.kt
@@ -0,0 +1,6 @@
+package taxi_together.taxi_together.carpoolApplication.domain.constant
+
+object CarpoolApplicationConstant {
+    const val CARPOOL_UUID = "carpool_uuid"
+    const val MEMBER_UUID = "member_uuid"
+}
