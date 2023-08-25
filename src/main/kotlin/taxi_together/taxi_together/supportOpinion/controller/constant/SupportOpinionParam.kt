051f44c8378cd67d443c4ac27d256852e85870a7
liveforone <yc4852@gmail.com>
2023-08-25 17:23:28 +0900

    Add support opinion param.

---
+++ src/main/kotlin/taxi_together/taxi_together/supportOpinion/controller/constant/SupportOpinionParam.kt
@@ -0,0 +1,8 @@
+package taxi_together.taxi_together.supportOpinion.controller.constant
+
+object SupportOpinionParam {
+    const val UUID = "uuid"
+    const val OPINION_TYPE = "opinionType"
+    const val WRITER_UUID = "writerUUID"
+    const val LAST_UUID = "lastUUID"
+}
