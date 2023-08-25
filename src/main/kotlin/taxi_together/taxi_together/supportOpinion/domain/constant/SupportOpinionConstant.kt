f0d383192b425e5559931b49c4a5859e0ccac2bf
liveforone <yc4852@gmail.com>
2023-08-25 17:23:19 +0900

    Add support opinion constant.

---
+++ src/main/kotlin/taxi_together/taxi_together/supportOpinion/domain/constant/SupportOpinionConstant.kt
@@ -0,0 +1,6 @@
+package taxi_together.taxi_together.supportOpinion.domain.constant
+
+object SupportOpinionConstant {
+    const val CONTENT_TYPE = "VARCHAR(300)"
+    const val SUPPORT_TYPE = "VARCHAR(8)"
+}
