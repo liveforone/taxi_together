c63669dca79d93159ccdaff04a9619a015a768fd
liveforone <yc4852@gmail.com>
2023-08-25 17:23:45 +0900

    Add support opinion controller log.

---
+++ src/main/kotlin/taxi_together/taxi_together/supportOpinion/controller/constant/SupportOpinionControllerLog.kt
@@ -0,0 +1,6 @@
+package taxi_together.taxi_together.supportOpinion.controller.constant
+
+enum class SupportOpinionControllerLog(val log: String) {
+    CREATE_SUCCESS("지원 의견 생성 성공"),
+    REMOVE_SUCCESS("지원 의견 삭제 성공")
+}
