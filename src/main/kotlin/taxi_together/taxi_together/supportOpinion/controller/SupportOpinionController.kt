b1124db2eae3ad575995b364eec80227d7efa933
liveforone <yc4852@gmail.com>
2023-08-25 17:23:50 +0900

    Add support opinion controller.

---
+++ src/main/kotlin/taxi_together/taxi_together/supportOpinion/controller/SupportOpinionController.kt
@@ -0,0 +1,86 @@
+package taxi_together.taxi_together.supportOpinion.controller
+
+import jakarta.validation.Valid
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.BindingResult
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import taxi_together.taxi_together.globalUtil.validateBinding
+import taxi_together.taxi_together.logger
+import taxi_together.taxi_together.supportOpinion.controller.constant.SupportOpinionControllerLog
+import taxi_together.taxi_together.supportOpinion.controller.constant.SupportOpinionParam
+import taxi_together.taxi_together.supportOpinion.controller.constant.SupportOpinionUrl
+import taxi_together.taxi_together.supportOpinion.controller.response.SupportOpinionResponse
+import taxi_together.taxi_together.supportOpinion.dto.request.CreateSupportOpinion
+import taxi_together.taxi_together.supportOpinion.dto.request.RemoveSupportOpinion
+import taxi_together.taxi_together.supportOpinion.service.command.SupportOpinionCommandService
+import taxi_together.taxi_together.supportOpinion.service.query.SupportOpinionQueryService
+import java.util.UUID
+
+@RestController
+class SupportOpinionController @Autowired constructor(
+    private val supportOpinionQueryService: SupportOpinionQueryService,
+    private val supportOpinionCommandService: SupportOpinionCommandService
+) {
+    @GetMapping(SupportOpinionUrl.DETAIL)
+    fun detail(@PathVariable(SupportOpinionParam.UUID) uuid: UUID): ResponseEntity<*> {
+        val supportOpinion = supportOpinionQueryService.getOneByUUID(uuid)
+        return SupportOpinionResponse.detailSuccess(supportOpinion)
+    }
+
+    @GetMapping(SupportOpinionUrl.ALL)
+    fun all(@RequestParam(SupportOpinionParam.LAST_UUID, required = false) lastUUID: UUID?): ResponseEntity<*> {
+        val supports = supportOpinionQueryService.getAllSupportOpinion(lastUUID)
+        return SupportOpinionResponse.allSuccess(supports)
+    }
+
+    @GetMapping(SupportOpinionUrl.BY_OPINION_TYPE)
+    fun byOpinionType(
+        @PathVariable(SupportOpinionParam.OPINION_TYPE) opinionType: String,
+        @RequestParam(SupportOpinionParam.LAST_UUID, required = false) lastUUID: UUID?
+    ): ResponseEntity<*> {
+        val supports = supportOpinionQueryService.getSupportOpinionsByType(opinionType, lastUUID)
+        return SupportOpinionResponse.byOpinionTypeSuccess(supports)
+    }
+
+    @GetMapping(SupportOpinionUrl.BY_WRITER)
+    fun byWriter(
+        @PathVariable(SupportOpinionParam.WRITER_UUID) writerUUID: UUID,
+        @RequestParam(SupportOpinionParam.LAST_UUID, required = false) lastUUID: UUID?
+    ): ResponseEntity<*> {
+        val supports = supportOpinionQueryService.getSupportOpinionsByWriter(writerUUID, lastUUID)
+        return SupportOpinionResponse.byWriterSuccess(supports)
+    }
+
+    @PostMapping(SupportOpinionUrl.CREATE)
+    fun create(
+        @RequestBody @Valid createSupportOpinion: CreateSupportOpinion,
+        bindingResult: BindingResult
+    ): ResponseEntity<*> {
+        validateBinding(bindingResult)
+
+        supportOpinionCommandService.createSupportOpinion(createSupportOpinion)
+        logger().info(SupportOpinionControllerLog.CREATE_SUCCESS.log)
+
+        return SupportOpinionResponse.createSuccess()
+    }
+
+    @DeleteMapping(SupportOpinionUrl.REMOVE)
+    fun remove(
+        @RequestBody @Valid removeSupportOpinion: RemoveSupportOpinion,
+        bindingResult: BindingResult
+    ): ResponseEntity<*> {
+        validateBinding(bindingResult)
+
+        supportOpinionCommandService.removeSupportOpinion(removeSupportOpinion)
+        logger().info(SupportOpinionControllerLog.REMOVE_SUCCESS.log)
+
+        return SupportOpinionResponse.removeSuccess()
+    }
+}
