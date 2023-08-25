de246bde43e462502d7183e98c00fff39c214d00
liveforone <yc4852@gmail.com>
2023-08-25 17:23:33 +0900

    Add support opinion url.

---
+++ src/main/kotlin/taxi_together/taxi_together/supportOpinion/controller/constant/SupportOpinionUrl.kt
@@ -0,0 +1,10 @@
+package taxi_together.taxi_together.supportOpinion.controller.constant
+
+object SupportOpinionUrl {
+    const val DETAIL = "/support/opinion/detail/{uuid}"
+    const val ALL = "/support/opinion/all"
+    const val BY_OPINION_TYPE = "/support/opinion/type/{opinionType}"
+    const val BY_WRITER = "/support/opinion/writer/{writerUUID}"
+    const val CREATE = "/support/opinion/create"
+    const val REMOVE = "/support/opinion/remove"
+}
