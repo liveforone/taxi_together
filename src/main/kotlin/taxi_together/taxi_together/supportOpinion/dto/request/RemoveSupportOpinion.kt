c4affb11142fd8dab5800e952617b50135939708
liveforone <yc4852@gmail.com>
2023-08-25 17:22:06 +0900

    Add remove support opinion dto.

---
+++ src/main/kotlin/taxi_together/taxi_together/supportOpinion/dto/request/RemoveSupportOpinion.kt
@@ -0,0 +1,11 @@
+package taxi_together.taxi_together.supportOpinion.dto.request
+
+import jakarta.validation.constraints.NotNull
+import java.util.UUID
+
+data class RemoveSupportOpinion(
+    @field:NotNull(message = "지원의견의 외부 식별자를 입력하세요.")
+    val uuid: UUID?,
+    @field:NotNull(message = "작성자의 외부 식별자를 입력하세요.")
+    val writerUUID: UUID?,
+)
