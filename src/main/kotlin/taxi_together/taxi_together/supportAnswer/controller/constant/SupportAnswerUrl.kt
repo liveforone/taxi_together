ea3b6140abcd5aa0d0b05e17a6ac297df7c3db77
liveforone <yc4852@gmail.com>
2023-08-30 13:17:11 +0900

    Add support answer url.

---
+++ src/main/kotlin/taxi_together/taxi_together/supportAnswer/controller/constant/SupportAnswerUrl.kt
@@ -0,0 +1,7 @@
+package taxi_together.taxi_together.supportAnswer.controller.constant
+
+object SupportAnswerUrl {
+    const val BY_SUPPORT_OPINION = "/support/answer/{supportOpinionId}"
+    const val CREATE = "/support/answer/create"
+    const val REMOVE = "/support/answer/remove"
+}
