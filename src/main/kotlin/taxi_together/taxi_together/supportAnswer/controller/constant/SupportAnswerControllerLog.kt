c265500c645b11d341556365637fa15724d51c46
liveforone <yc4852@gmail.com>
2023-08-30 13:17:23 +0900

    Add support answer controller log.

---
+++ src/main/kotlin/taxi_together/taxi_together/supportAnswer/controller/constant/SupportAnswerControllerLog.kt
@@ -0,0 +1,6 @@
+package taxi_together.taxi_together.supportAnswer.controller.constant
+
+enum class SupportAnswerControllerLog(val log: String) {
+    CREATE_SUCCESS("지원 의견 답변 생성 성공"),
+    REMOVE_SUCCESS("지원 의견 답변 삭제 성공")
+}
